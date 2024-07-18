029afbe2f7d5eaf0b2b3ee9424507a4bcbd88801
HariPrasanth <hprasanth50@gmail.com>
2024-07-18 15:24:50 +0530

    bug fixes

--- uikit/src/main/java/com/boilerplate/uikit/views/textviews/text24/TextViewBoldYellow24.kt
+++ uikit/src/main/java/com/boilerplate/uikit/views/textviews/text24/TextViewBoldYellow24.kt
@@ -14,6 +14,6 @@ class TextViewBoldYellow24 : AppCompatTextView {
     constructor(context: Context) : super(context)
 
     init {
-        setTextAppearance(R.style.textview_bold_white_24)
+        setTextAppearance(R.style.textview_bold_yellow_24)
     }
 }
